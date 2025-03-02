b893f8bee85088152fe0854ef67747da8e4601bf
Henry <henry.pap@consid.se>
2025-03-03 00:10:35 +0100

    add: unit test on vector math

--- packages/game/vector/tests/vector/unit.test.js
+++ packages/game/vector/tests/vector/unit.test.js
@@ -20,4 +20,45 @@ describe('@papit/game-vector unit tests', () => {
       assert.strictEqual(a.z, v.z);
     }
   });
+
+
+  it('should add successfully', () => {
+    const a = new Vector(1, 2);
+    a.add(2);
+
+    assert.strictEqual(a.x, 3);
+    assert.strictEqual(a.y, 4);
+
+    a.add(1, 0);
+
+    assert.strictEqual(a.x, 4);
+    assert.strictEqual(a.y, 4);
+  });
+
+  it('should subtract successfully', () => {
+    const a = new Vector(1, 2);
+    a.sub(2);
+
+    assert.strictEqual(a.x, -1);
+    assert.strictEqual(a.y, 0);
+
+    a.sub(1, 0);
+
+    assert.strictEqual(a.x, -2);
+    assert.strictEqual(a.y, 0);
+  });
+
+
+  it('should multiply successfully', () => {
+    const a = new Vector(1, 2);
+    a.mul(2);
+
+    assert.strictEqual(a.x, 2);
+    assert.strictEqual(a.y, 4);
+
+    a.mul(2, 0);
+
+    assert.strictEqual(a.x, 4);
+    assert.strictEqual(a.y, 0);
+  });
 });
